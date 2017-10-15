cd77190b6a326939af6caaacb0083ef798a57f20
Florian Lenz <flenz81@googlemail.com>
2017-10-15 21:06:16 +0200

    [panthalassa] added panthalassa to pangea

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-
+import * as panthalassa from 'panthalassa-ts';
 import Main from './src/index.mobile';
+
+console.log(panthalassa);
 
 export default class App extends Component { // eslint-disable-line react/prefer-stateless-function
   render() {
