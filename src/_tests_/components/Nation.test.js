022d0e8a60470b7046da484ba2701641365b4cd7
Jetpack <romcaantl@gmail.com>
2017-10-20 09:55:53 +0200

    Add tests for components and App container Update packages into newest versions (stable React 16, etc.) Add _mocks_ for mocking types in tests

--- src/_tests_/components/Nation.test.js
+++ src/_tests_/components/Nation.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import { Text } from 'react-native';
 
 import Nation from '../../components/Nation';
@@ -21,9 +21,10 @@ describe('<Nation />', () => {
 
     const textNodes = wrapper.find(Text);
     expect(textNodes.length).toBe(6);
+    const nodes = textNodes.getElements();
 
-    expect(textNodes.nodes[1].props.children).toEqual(nationData.name);
-    expect(textNodes.nodes[3].props.children).toEqual(nationData.type);
-    expect(textNodes.nodes[5].props.children).toEqual(nationData.reputation);
+    expect(nodes[1].props.children).toEqual(nationData.name);
+    expect(nodes[3].props.children).toEqual(nationData.type);
+    expect(nodes[5].props.children).toEqual(nationData.reputation);
   })
 });
